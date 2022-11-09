1e25c98c0cde29430ca8b6b7f7a8a5f97fbe1ebc
FSV.lab <fsaldana7@misena.edu.co>
2022-11-08 20:09:24 -0500

    Proyecto en java web Gestión inventario

---
+++ src/java/jpa/sessions/ClienteFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpa.sessions;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import jpa.entities.Cliente;
+
+/**
+ *
+ * @author ferch
+ */
+@Stateless
+public class ClienteFacade extends AbstractFacade<Cliente> {
+
+    @PersistenceContext(unitName = "Gestion_InventarioPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ClienteFacade() {
+        super(Cliente.class);
+    }
+    
+}
